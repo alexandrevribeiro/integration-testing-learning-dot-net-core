b1d6093d3d6aea8bbdc87e6a62fb9972c3111cc5
Alexandre Ribeiro <alexandre.ribeiro@thomsonreuters.com>
2021-06-09 21:16:47 -0300

    TDD - First test case and making it pass (red->green)

---
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
+{
+    public class StockControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public StockControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            //factory.ClientOptions.BaseAddress = new Uri("http://localhost/api/stock");
+
+            _httpClient = factory.CreateDefaultClient(new Uri("http://localhost/api/stock/"));
+        }
+
+        [Fact]
+        public async Task GetStockTotal_ReturnsSuccessStatusCode()
+        {
+            var response = await _httpClient.GetAsync("total");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
