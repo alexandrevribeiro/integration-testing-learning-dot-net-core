6c18273077e8e9d48418f92822d0a9607ff7b70c
Alexandre Ribeiro <alexandre.ribeiro@thomsonreuters.com>
2021-06-13 13:03:03 -0300

    TDD - Creating a failing test with a FAKE dependency (red)

--- test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
@@ -5,6 +5,11 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using TennisBookings.Merchandise.Api.Data.Dto;
+using TennisBookings.Merchandise.Api.External.Database;
+using TennisBookings.Merchandise.Api.IntegrationTests.Fakes;
 using TennisBookings.Merchandise.Api.IntegrationTests.Models;
 using Xunit;
 
@@ -13,11 +18,13 @@ namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
     public class StockControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly HttpClient _httpClient;
+        private readonly WebApplicationFactory<Startup> _factory;
 
         public StockControllerTests(WebApplicationFactory<Startup> factory)
         {
             factory.ClientOptions.BaseAddress = new Uri("http://localhost/api/stock/");
             _httpClient = factory.CreateClient();
+            _factory = factory;
         }
 
         [Fact]
@@ -27,6 +34,29 @@ namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
 
             Assert.NotNull(model);
             Assert.True(model.StockItemTotal > 0);            
+        }
+
+        [Fact]
+        public async Task GetStockTotal_ReturnsExpectedStockQuantity()
+        {
+            var cloudDatabase = new FakeCloudDatabase(new []
+            {
+                new ProductDto { StockCount = 200 },
+                new ProductDto { StockCount = 500 },
+                new ProductDto { StockCount = 300 }
+            });
+
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<ICloudDatabase>(cloudDatabase);
+                });
+            }).CreateClient();
+
+            var model = await client.GetFromJsonAsync<ExpectedStockTotalOutputModel>("total");
+
+            Assert.Equal(1000, model.StockItemTotal);
         }
 
         #region Previous tests simplified into the new "GetStockTotal_ReturnsExpectedJson" above:
