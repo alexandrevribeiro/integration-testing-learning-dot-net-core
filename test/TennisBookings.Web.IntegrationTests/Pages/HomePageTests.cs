237c31d7514cbc083fc1cc455ebd6c4c03ac5bb7
Alexandre Ribeiro <alexandre.ribeiro@thomsonreuters.com>
2021-06-14 21:22:44 -0300

    Adding integration test that depends on Configurations by using In-memory configuration provider

--- test/TennisBookings.Web.IntegrationTests/Pages/HomePageTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Pages/HomePageTests.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using Microsoft.Extensions.Configuration;
 using TennisBookings.Web.IntegrationTests.Helpers;
 using Xunit;
 
@@ -11,7 +15,7 @@ namespace TennisBookings.Web.IntegrationTests.Pages
         public HomePageTests(CustomWebApplicationFactory<Startup> factory)
         {
             _factory = factory;
-        }        
+        }
 
         [Fact]
         public async Task Get_ReturnsPageWithExpectedH1()
@@ -29,6 +33,43 @@ namespace TennisBookings.Web.IntegrationTests.Pages
             var h1 = content.QuerySelector("h1");
 
             Assert.Equal("Welcome to Tennis by the Sea!", h1.TextContent);
+        }
+
+        public static IEnumerable<object[]> ConfigVariations => new List<object[]>
+        {
+            // 1st param: Config "Features > WeatherForecasting > EnableWeatherForecast"
+            // 2nd param: Config "Features > HomePage > EnableWeatherForecast"
+            // 3rd param: whether the "Weather Forecast" should be displayed or not
+            new object[] { false, false, false },
+            new object[] { true, false, false },
+            new object[] { false, true, false },
+            new object[] { true, true, true }
+        };
+
+        [Theory]
+        [MemberData(nameof(ConfigVariations))]
+        public async Task HomePageIncludesForecast_ForExpectedConfigVariations(bool globalEnabled,
+            bool pageEnabled, bool shouldDisplayForecast)
+        {
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((ctx, config) =>
+                {
+                    config.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { "Features:WeatherForecasting:EnableWeatherForecast", globalEnabled.ToString() },
+                        { "Features:HomePage:EnableWeatherForecast", pageEnabled.ToString() }
+                    });
+                });
+            }).CreateClient();
+
+            var response = await client.GetAsync("/");
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+
+            var forecastDiv = content.All.SingleOrDefault(e => e.Id == "weather-forecast" &&
+                e.LocalName == TagNames.Div);
+
+            Assert.Equal(shouldDisplayForecast, forecastDiv != null);
         }
     }
 }
