23fae2e1794e31c40972d550f7bc1d5df26ded9e
Alexandre Ribeiro <alexandre.ribeiro@thomsonreuters.com>
2021-06-16 08:47:53 -0300

    Refactoring tests that depend on DbContext - Creating Extension methods to reuse repeated code

---
+++ test/TennisBookings.Web.IntegrationTests/Extensions/WebApplicationFactoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TennisBookings.Web.Data;
+using TennisBookings.Web.IntegrationTests.Pages;
+
+namespace TennisBookings.Web.IntegrationTests.Extensions
+{
+    public static class WebApplicationFactoryExtensions
+    {
+        public static HttpClient CreateClientWithMemberAndDbSetup<TLogCategoryName>(this WebApplicationFactory<Startup> factory,
+            Action<TennisBookingDbContext> configure)
+        {
+            var client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.WithMemberUser().ConfigureTestDatabase<TLogCategoryName>(configure);
+            }).CreateClient();
+
+            // (Optional) We can also add an authorization header to the default headers for the
+            // test client so that each request specifies the name of the Test scheme.
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
+
+            return client;
+        }
+    }
+}
+
--- test/TennisBookings.Web.IntegrationTests/Pages/BookingsPageTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Pages/BookingsPageTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TennisBookings.Web.Data;
+using TennisBookings.Web.IntegrationTests.Extensions;
 using TennisBookings.Web.IntegrationTests.Helpers;
 using Xunit;
 
@@ -21,6 +22,77 @@ namespace TennisBookings.Web.IntegrationTests.Pages
             factory.ClientOptions.BaseAddress = new Uri("http://localhost/bookings");
             _factory = factory;
         }
+
+        [Fact]
+        public async Task NoBookingsTableOnPage_WhenUserHasNoBookings()
+        {
+            // Creates a client registering a Member (Test Authentication Schema) and
+            // setting up the DbContext to reset the DB for this Test
+            var client = _factory.CreateClientWithMemberAndDbSetup<BookingsPageTests>(db => 
+                DatabaseHelper.ResetDbForTests(db));
+
+           var response = await client.GetAsync("");
+
+            response.AssertOk();
+
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+            var table = content.QuerySelector("table");
+
+            Assert.Null(table);
+        }
+
+        [Fact]
+        public async Task ExpectedBookingsTableRowsOnPage_WhenUserHasBooking()
+        {
+            var startDate = FixedDateTime.UtcNow.AddDays(5);
+
+            // Creates a client registering a Member (Test Authentication Schema) and
+            // setting up the DbContext for this Test
+            var client = _factory.CreateClientWithMemberAndDbSetup<BookingsPageTests>(db =>
+            {
+                DatabaseHelper.ResetDbForTests(db);
+
+                var member = db.Members.Find(1);
+                var court = db.Courts.Find(1);
+
+                db.CourtBookings.Add(new CourtBooking
+                {
+                    Court = court,
+                    Member = member,
+                    StartDateTime = startDate,
+                    EndDateTime = startDate.AddHours(2)
+                });
+
+                db.SaveChanges();
+            });
+
+            var response = await client.GetAsync("");
+
+            response.AssertOk();
+
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+            var table = content.QuerySelector("table");
+            var tableBody = table.QuerySelector("tbody");
+            var rows = tableBody.QuerySelectorAll("tr");
+
+            Assert.Single(rows);
+
+            // This following approach provides more asurance of the content provided in the oage,
+            // BUT it's more coupled to the HTML output, which may require more work to maintain
+            // So it's a tradeoff we're making
+            Assert.Collection(rows, r =>
+            {
+                var cells = r.QuerySelectorAll("td");
+
+                Assert.Equal(startDate.ToString("D"), cells[0].TextContent);
+                Assert.Equal("Court 1", cells[1].TextContent);
+                Assert.Equal("10 am to 12 pm", cells[2].TextContent);
+            });
+        }
+
+        #region Previous test methods before refactoring them
+
+        /*
 
         [Fact]
         public async Task NoBookingsTableOnPage_WhenUserHasNoBookings()
@@ -143,6 +215,9 @@ namespace TennisBookings.Web.IntegrationTests.Pages
                 Assert.Equal("10 am to 12 pm", cells[2].TextContent);
             });
         }
+
+        */
+        #endregion
     }
 }
 
