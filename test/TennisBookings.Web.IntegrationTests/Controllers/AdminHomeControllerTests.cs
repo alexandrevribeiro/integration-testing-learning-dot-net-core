387b9b24a9455b5d751ea73bf2e510ddd436166b
Alexandre Ribeiro <alexandre.ribeiro@thomsonreuters.com>
2021-06-15 21:38:12 -0300

    Adding Test to ensure Unauthenticated requests redirect to Login page

--- test/TennisBookings.Web.IntegrationTests/Controllers/AdminHomeControllerTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Controllers/AdminHomeControllerTests.cs
@@ -12,8 +12,21 @@ namespace TennisBookings.Web.IntegrationTests.Controllers
 
         public AdminHomeControllerTests(CustomWebApplicationFactory<Startup> factory)
         {
+            // Disabling the default "redirect" behavior (which is following any rediorects
+            // until it gets to a final resource), for the Test to assert on the response itself
             factory.ClientOptions.AllowAutoRedirect = false;            
             _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_SecurePageIsForbiddenForAnUnauthenticatedUser()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Admin");
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.StartsWith("http://localhost/identity/account/login", response.Headers.Location.OriginalString.ToLower());
         }
     }
 }
