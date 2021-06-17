1e21f4cc7aedde0eddf62a4e174c8d245617d171
Alexandre Ribeiro <alexandre.ribeiro@thomsonreuters.com>
2021-06-16 21:58:45 -0300

    Duplicating the "Post_SendsEmail" test, but using Mock instead of Fake

--- test/TennisBookings.Web.IntegrationTests/Pages/EnquiryPageTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Pages/EnquiryPageTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AngleSharp.Html.Dom;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using TennisBookings.Web.Core;
 using TennisBookings.Web.IntegrationTests.Extensions;
 using TennisBookings.Web.IntegrationTests.Fakes;
@@ -69,6 +70,58 @@ namespace TennisBookings.Web.IntegrationTests.Pages
             response.AssertOk();
 
             Assert.Single(emailService.SentEmails);
+        }
+
+
+        public async Task Post_SendsEmail_WITH_MOCK()
+        {
+            // SAME TEST above, but using Mock instead of Fake
+            var emailService = new Mock<IEmailService>();
+
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton(emailService.Object);
+                });
+            }).CreateClient();
+
+            // ASP.NET Core uses "Antiforgery Tokens" to prevent CSRF attacks.
+            // These are included in forms and as a cookie. 
+            // So, just making a POST request will fail since it doesn't have the token
+            // neither the cookie... instead, it's necessary to GET the form page and 
+            // use to make a POST request (the cookies will be automatically handled by the client)
+
+            var getResponse = await client.GetAsync("enquiry");
+            using var content = await HtmlHelpers.GetDocumentAsync(getResponse);
+            var form = (IHtmlFormElement)content.QuerySelector("form");
+
+            if (form["Email"] is IHtmlInputElement email)
+                email.Value = "example@example.com";
+
+            if (form["Subject"] is IHtmlInputElement subject)
+                subject.Value = "Testing";
+
+            if (form["Message"] is IHtmlTextAreaElement message)
+                message.Value = "This is a test message.";
+
+            var button = (IHtmlButtonElement)content.QuerySelector("button");
+            var formSubmission = form.GetSubmission(button);
+            var target = (Uri)formSubmission.Target;
+
+            var request = new HttpRequestMessage(new HttpMethod(formSubmission.Method.ToString()), target)
+            {
+                Content = new StreamContent(formSubmission.Body)
+            };
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await client.SendAsync(request);
+
+            response.AssertOk();
+
+
+            emailService.Verify(s => s.SendAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
+                Times.Once);
         }
     }
 }
